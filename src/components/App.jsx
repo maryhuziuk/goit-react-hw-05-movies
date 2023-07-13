1b50db89f5cc6f52789f7be4aae68c14a014edaa
maryhuziuk <116351558+maryhuziuk@users.noreply.github.com>
2023-07-13 13:55:24 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,12 +14,12 @@ export const App = () => {
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
+        <Route path="*" element={<Navigate to="/" />} />
           <Route index element={<Home />} />
           <Route path="movies" element={<Movies />} />
           <Route path="movies/:movieId" element={<MovieDetails />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
-            <Route path="*" element={<Navigate to="/" />} />
           </Route>
         </Route>
       </Routes>
